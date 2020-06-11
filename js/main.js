8806cc3d3bad9e9805157d99500c9c9e1824a1be
subarna00 <59225774+subarna00@users.noreply.github.com>
2020-06-11 13:59:38 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function initMap(){
+    var location = {lat:27.708065, lng:85.337099};
+    var map = new google.maps.Map(document.getElementById("map"),{
+        zoom:16,
+        center: location
+    });
+}
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY >150){
+       document.querySelector('#navbar').style.opacity = 0.9;
+    }else{
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+//Smooth scrolling
+$('#navbar a').on('click',function(event){
+  if(this.hash !==''){
+      event.preventDefault();
+      const hash = this.hash;
+ 
+  $('html,body').animate(
+      {
+          scrollTop: $(hash).offset().top -20
+      },
+      800
+  );
+}
+});
